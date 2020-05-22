763609b16f72aba4dec72f5c6b27c18f84223f43
premb1 <prem.psna2008@gmail.com>
2020-05-22 21:27:25 +0530

    First commit

---
+++ Calclations.java
@@ -0,0 +1,28 @@
+package calculator;
+
+public class Calclations {
+	
+
+	public int getAdd(int x,int y)
+	{
+		int add=x+y;
+		System.out.println("addition"+add);
+		return add;
+		
+	}
+	public void getSub(int x,int y)
+	{
+		int sub=x-y;
+		System.out.println("Sub"+sub);
+		
+	}
+	public void getMul(int x,int y)
+	{
+		int mul=x*y;
+		System.out.println("Multiplication"+mul);
+		
+	}
+	
+	
+
+}
